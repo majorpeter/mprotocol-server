c0df817f8b3dbed1feb96ac0df5813db636948dd
Major Péter <majorpeter29@gmail.com>
2015-06-25 20:42:16 +0200

    implemented basic node/protocolparser sturcture with c++11 lambda setters/getters in properties

--- ProtocolParser.cpp
+++ ProtocolParser.cpp
@@ -3,15 +3,20 @@
 #include <stdio.h>
 #include <string.h>
 
-ProtocolParser::ProtocolParser(AbstractSerialInterface* serialInterface): serialInterface(serialInterface) {
-
-}
+ProtocolParser::ProtocolParser(AbstractSerialInterface* serialInterface): serialInterface(serialInterface) {}
 
 void ProtocolParser::listen() {
     serialInterface->listen();
 }
 
 void ProtocolParser::receiveString(char* s) {
+    ProtocolResult_t result = parseString(s);
+    if (result != ProtocolResult_Ok) {
+        reportError(result);
+    }
+}
+
+ProtocolResult_t ProtocolParser::parseString(char *s) {
     // decode protocol function
     enum {UNKNOWN, GET, SET, CALL} decodedFunc = UNKNOWN;
 
@@ -27,39 +32,94 @@ void ProtocolParser::receiveString(char* s) {
     }
 
     if (decodedFunc == UNKNOWN) {
-        reportError(ProtocolResult_UnknownFunc);
-        return;
+        return ProtocolResult_UnknownFunc;
     }
 
     // decode target node
     Node* node = NULL;
     char *p;
     if (s[0] != '/') {
-        reportError(ProtocolResult_NodeNotFound);
-        return;
+        return ProtocolResult_NodeNotFound;
     }
     node = RootNode::getInstance();
     s++;
     for (;;) {
         p = strchr(s, '/');
-        if (p != NULL) {
-            *p ='\0';
-            node = node->getChildByName(s);
-            if (node == NULL) {
-                reportError(ProtocolResult_NodeNotFound);
-                return;
-            }
-            s = p+1;
-        } else {
-            p = strchr(s, '.');
-            if (p != NULL) {
+        if (p == NULL) {
+            break;
+        }
+        *p ='\0';
+        node = node->getChildByName(s);
+        if (node == NULL) {
+            return ProtocolResult_NodeNotFound;
+        }
+        s = p+1;
+    }
+    if (node == NULL) {
+        return ProtocolResult_NodeNotFound;
+    }
 
-            } else {
-                reportError(ProtocolResult_SyntaxError);
-                return;
-            }
+    if (s[0] == '\0') {
+        if (decodedFunc == GET) {
+            listNode(node);
+            return ProtocolResult_Ok;
+        } else {
+            return ProtocolResult_InvalidFunc;
         }
     }
+
+    p = strchr(s, '.');
+    if (p == NULL) {
+        if (decodedFunc == GET) {
+            node = node->getChildByName(s);
+            if (node != NULL) {
+                listNode(node);
+                return ProtocolResult_Ok;
+            }
+        }
+        return ProtocolResult_SyntaxError;
+    }
+    *p = '\0';
+    node = node->getChildByName(s);
+    s = p + 1;
+    if (node == NULL) {
+        return ProtocolResult_NodeNotFound;
+    }
+
+    p = strchr(s, '=');
+    if (p != NULL) {
+        *p = '\0';
+    }
+    const Property_t *prop = node->getPropertyByName(s);
+    s = p + 1;
+    if (prop == NULL) {
+        return ProtocolResult_PropertyNotFound;
+    }
+
+    switch (decodedFunc) {
+    default:
+    case GET:
+        if (prop->type == PropertyType_Method) {
+            return ProtocolResult_InvalidFunc;
+        }
+        getProperty(node, prop);
+        return ProtocolResult_Ok;
+    case SET:
+        if (prop->accessLevel != PropAccessLevel_ReadWrite) {
+            return ProtocolResult_InvalidFunc;
+        }
+        setProperty(node, prop, s);
+        return ProtocolResult_Ok;
+    case CALL:
+        if (prop->type != PropertyType_Method) {
+            return ProtocolResult_InvalidFunc;
+        }
+        setProperty(node, prop, s);
+        return ProtocolResult_Ok;
+    }
+
+
+    //return ProtocolResult_UnknownFunc;
 }
 
 void ProtocolParser::reportError(ProtocolResult_t errorCode) {
@@ -72,12 +132,69 @@ void ProtocolParser::reportError(ProtocolResult_t errorCode) {
     fputs("\n", stdout);
 }
 
+void ProtocolParser::listNode(Node *node) {
+    puts("{");
+    Node* n = node->getFirstChild();
+    while (n != NULL) {
+        fputs("N ", stdout);
+        fputs(n->getName(), stdout);
+        fputs("\n", stdout);
+        n = n->getNextSibling();
+    }
+
+    const Property_t** props = node->getProperties();
+    uint16_t propsCount = node->getPropertiesCount();
+    for (uint16_t i = 0; i < propsCount; i++) {
+        puts(props[i]->name);
+    }
+    puts("}");
+}
+
+void ProtocolParser::getProperty(Node *node, const Property_t *prop) {
+    char buffer[256];
+    prop->getter(node, buffer);
+    switch (prop->type) {
+    case PropertyType_Uint32: sprintf(buffer, "%u", *((unsigned int*)buffer)); break;
+    case PropertyType_Int32: sprintf(buffer, "%d", *((int*)buffer)); break;
+    case PropertyType_Bool: strcpy(buffer, buffer[0] ? "true" : "false"); break;
+    case PropertyType_String: break;
+    case PropertyType_Method: strcpy(buffer, "METHOD GETTER CALLED!"); break;
+    }
+    puts(buffer);
+}
+
+void ProtocolParser::setProperty(Node *node, const Property_t *prop, const char *value) {
+    char buffer[256] = {0};
+    switch (prop->type) {
+    case PropertyType_Uint32: sscanf(value, "%u", (unsigned int*)buffer); break;
+    case PropertyType_Int32: sscanf(value, "%d", (int*)buffer); break;
+    case PropertyType_Bool:
+        if ((strcmp(value, "true") == 0) || (strcmp(value, "1") == 0))
+            buffer[0] = 1;
+        else if ((strcmp(value, "false") == 0) || (strcmp(value, "0") == 0))
+            buffer[0] = 0;
+        else
+            return;
+        break;
+    case PropertyType_String:
+    case PropertyType_Method:
+        strcpy(buffer, value);
+        break;
+    }
+    prop->setter(node, buffer);
+    if (prop->type == PropertyType_Method) {
+        fputs(buffer, stdout);
+    }
+}
+
 const char* ProtocolParser::resultToStr(ProtocolResult_t result) {
     switch (result) {
     case ProtocolResult_Ok: return "Ok";
     case ProtocolResult_UnknownFunc: return "Unknown function";
     case ProtocolResult_NodeNotFound: return "Node not found";
+    case ProtocolResult_PropertyNotFound: return "Property not found";
     case ProtocolResult_SyntaxError: return "Syntax Error";
+    case ProtocolResult_InvalidFunc: return "Invalid function";
     }
     return "Unknown Error code!";
 }
--- ProtocolParser.h
+++ ProtocolParser.h
@@ -3,21 +3,31 @@
 
 #include "AbstractSerialInterface.h"
 #include "AbstractUpLayer.h"
+#include "Nodes/Properties.h"
+
+class Node;
 
 typedef enum {
     ProtocolResult_Ok,
     ProtocolResult_UnknownFunc,
     ProtocolResult_NodeNotFound,
+    ProtocolResult_PropertyNotFound,
     ProtocolResult_SyntaxError,
+    ProtocolResult_InvalidFunc,
 } ProtocolResult_t;
 
 class ProtocolParser: public AbstractUpLayer {
     AbstractSerialInterface* serialInterface;
+
+    void listNode(Node *node);
+    void getProperty(Node *node, const Property_t *prop);
+    void setProperty(Node *node, const Property_t *prop, const char* value);
 public:
     ProtocolParser(AbstractSerialInterface* serialInterface);
     virtual ~ProtocolParser() {}
     void listen();
     virtual void receiveString(char* s);
+    ProtocolResult_t parseString(char* s);
     void reportError(ProtocolResult_t errorCode);
     static const char* resultToStr(ProtocolResult_t result);
 };
